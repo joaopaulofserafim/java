4ef8c77f6fb288270a177bcde6ac895eeb6ff785
joaopaulofserafim <jpfserafim72@gmail.com>
2025-03-31 11:50:08 -0300

    f

---
+++ src/ado7.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+import java.util.Collections;
+ 
+public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+
+    System.out.println("*****ADOO7*****");
+
+    System.out.println("Qual é o principal objetivo do design de UX?");
+    System.out.println("a) Criar interfaces visualmente atraentes");
+    System.out.println("b) Melhorar a experiência do usuário com o produto");
+    System.out.println("c) Focar apenas na estética do produto");
+    System.out.println("d) Tornar o produto mais barato de ser produzido");
+    System.out.println("e) Definir o layout de cada página do site");
+
+
+
+
+
+
+
+
+
+}
