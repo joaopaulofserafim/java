6a1d74f7880ff037348fd787777ed5aae50282b9
joaopaulofserafim <jpfserafim72@gmail.com>
2025-04-03 15:00:13 -0300

    ado__&

--- src/EX1_SALA 1.java
+++ src/EX1_SALA 1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package exercicios_sala_ALP;
+
 import java.util.Scanner;
 
 /**
--- src/ado7.java
+++ src/ado7.java
@@ -1,26 +1,74 @@
-
-
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Collections;
- 
+import java.util.List;
+
+
+public class ado7 {
 public static void main(String[] args) {
     Scanner input = new Scanner (System.in);
 
-    System.out.println("*****ADOO7*****");
+    System.out.println("*****ADOO7*****\n ");
 
-    System.out.println("Qual é o principal objetivo do design de UX?");
+    System.out.println("Qual é o principal objetivo do design de UX? \n ");
+
+    boolean certo = false;
+
+    List<String> opcoes = Arrays.asList(
+        "a) Criar interfaces visualmente atraentes\n",
+        "b) Melhorar a experiência do usuário com o produto\n",
+        "c) Focar apenas na estética do produto\n",
+        "d) Tornar o produto mais barato de ser produzido\n",
+        "e) Definir o layout de cada página do site \n"
+        );
+        
+        System.out.println(opcoes);
+
+        
+    do {
+
+
+
+    /*System.out.println("Qual é o principal objetivo do design de UX? \n ");
     System.out.println("a) Criar interfaces visualmente atraentes");
     System.out.println("b) Melhorar a experiência do usuário com o produto");
     System.out.println("c) Focar apenas na estética do produto");
     System.out.println("d) Tornar o produto mais barato de ser produzido");
-    System.out.println("e) Definir o layout de cada página do site");
+    System.out.println("e) Definir o layout de cada página do site \n");
+*/
+
+    System.out.println("Escolha uma opção: \n");
+    String resposta = input.next();
+
+    switch (resposta) {
+        case "a":
+        case "A":
+        case "b":
+        case "B":
+        case "d":
+        case "D":
+        case "e":
+        case "E":
+
+        System.out.println("\nResposta incorreta!!!\nTente Novamente \n ");
+        Collections.shuffle(opcoes);
+            
+            break;
+
+            case "c":
+            case "C":
+            System.out.println("Resposta correta!! \n Parabéns!!");
+            certo = true;
+            break;
+    
+        default:
+        System.out.println("Opção Invalida");
+            break;
+    }
+
+    }while (!certo);
 
 
-
-
-
-
-
-
+}
 
 }
