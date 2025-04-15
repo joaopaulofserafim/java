c92ce9068cb87e863f1d59dec2e186fbe89b5ca1
joaopaulofserafim <jpfserafim72@gmail.com>
2025-04-15 11:56:50 -0300

    treinando

---
+++ treinando.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.Scannner;
+public class treinando {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        
+    }
+}
