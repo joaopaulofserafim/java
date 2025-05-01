05549de82deedb50f40a61df587302a6e7eaeed7
joaopaulofserafim <jpfserafim72@gmail.com>
2025-05-01 09:51:20 -0300

    treinando

--- src/treinando.java
+++ src/treinando.java
@@ -1,8 +1,46 @@
 import java.util.Scanner;
 public class treinando {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        
+
+    public static int numeroUm(){
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Digite um numero inteiro: ");
+        int n1 = input.nextInt();
+        return n1;
     }
+
+    public static int numeroDois(){
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Digite um numero inteiro: ");
+        int n2 = input.nextInt();
+        return n2;
+    }
+
+    public static int processamento (int n1, int n2){
+    Scanner input = new Scanner (System.in);
+
+    if (n1 > n2) {
+        System.out.println("Maior numero: ");
+        return n1;
+
+    }else if (n1 == n2){
+        System.out.println("Numero são iguais");
+        return n1;
+    }else {
+        System.out.println("Maior numero: ");
+        return n2;
+    }
+    }
+
+    public static void main(String[] args) {
+        int n1 = numeroUm();
+        int n2 = numeroDois();
+
+        int processa = processamento(n1, n2);
+        System.out.println(processa);
+    }
+
+
+
 }
