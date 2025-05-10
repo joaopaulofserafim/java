8e8e61768996b2f16f69d1409ecc9f82c3338708
JOAO PAULO FIGUEIREDO SERAFIM <joao.pfserafim1@senacsp.edu.br>
2025-05-09 21:11:42 -0300

    feat: alteracao ADO_10_2

--- src/ADO_10.java
+++ src/ADO_10.java
@@ -1,11 +1,23 @@
 /*Um jogo bem simples consiste em tentar adivinhar um valor inteiro aleatório
-escolhido entre 1 e 100 por um computador. O usuário/jogador terá apenas 5
-chances para tentar adivinhar o valor. A cada tentativa do jogador, caso ele não
+escolhido entre 1 e 100 por um computador  
+(random 1 a 100).
+
+O usuário/jogador terá apenas 5
+chances para tentar adivinhar o valor for 
+(incrementa). 
+
+A cada tentativa do jogador, caso ele não
 acerte, o programa deve informar se o valor digitado é maior ou menor que o
-valor escolhido pelo computador. E caso o valor digitado pelo jogador tenha
+valor escolhido pelo computador
+if / else if . 
+
+E caso o valor digitado pelo jogador tenha
 diferença em módulo igual a 1 com o valor correto deverá ser impresso “TÁ
 QUENTE!” (por exemplo, o computador escolheu 42 e o jogador digitou 41
-ou 43). No final, deverá ser impresso “Parabéns, você ganhou o jogo!”, se o
+ou 43).
+if / else if . 
+
+No final, deverá ser impresso “Parabéns, você ganhou o jogo!”, se o
 jogador conseguiu acertar o valor escolhido pelo computador, ou “Game
 Over!”, caso contrário.
 
