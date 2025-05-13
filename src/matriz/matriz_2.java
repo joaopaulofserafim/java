1423e3ffaf5470fcf5271af24032cd25ab8b00df
joaopaulofserafim <jpfserafim72@gmail.com>
2025-05-13 10:12:03 -0300

    Organizando

---
+++ src/matriz/matriz_2.java
@@ -0,0 +1,108 @@
+package matriz;
+
+import java.util.Scanner;
+public class matriz_2 {
+    
+
+    public static int [][] criarMatriz(int lin, int col){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("---Criando matriz---");
+
+        // instanciando
+
+        int [][] matriz = new int [lin][col];
+
+        for (int i = 0 ; i < matriz.length ; i++ ){
+            for(int j = 0 ; j < matriz[i].length; j++){
+                System.out.printf("Matriz [%d][%d]: ", i, j);  //rever dps, praticar
+                matriz[i][j] = input.nextInt();
+            }
+        }
+        return matriz;
+    }
+
+    public static void imprimir(int [][] matriz){
+        System.out.println("---imprimindo a matriz---");
+
+        for (int i = 0 ; i < matriz.length ; i++ ){
+            for(int j = 0 ; j < matriz[i].length; j++){
+                System.out.printf("Matriz [%d][%d]: %d\n", i, j, matriz[i][j]);
+            }
+
+        }
+
+    }
+
+    public static int somatorio(int [][] matriz){
+        System.out.println("---somatorio---");
+
+        int soma = 0;
+
+        for (int i = 0 ; i < matriz.length ; i++ ){
+            for(int j = 0 ; j < matriz[i].length; j++){
+
+                soma = soma + matriz[i][j];
+
+            }
+
+        }
+        return soma;
+    }
+
+    public static int qntdNegativos(int [][]matriz){
+        System.out.println("---Quantidade negativos---");
+
+        int contador = 0;
+
+        for (int i = 0 ; i < matriz.length ; i++ ){
+            for(int j = 0 ; j < matriz[i].length; j++){
+                if (matriz[i][j] < 0) {
+
+                   contador ++;
+                    
+                }
+            }
+
+        }
+        return contador;
+
+    }
+
+    public static int[] definirTamanho(){
+        System.out.println("---Definindo tamanho---");
+        Scanner input = new Scanner(System.in);
+
+        int [] vetor = new int[2];
+
+        System.out.println("Linhas: ");
+        vetor[0] = input.nextInt();                 // usuario defeine tamanho do array
+
+        System.out.println("Coluna: ");
+        vetor[1] = input.nextInt();
+
+        return vetor;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int [] v = definirTamanho();
+
+        int [][] matriz = criarMatriz(v[0], v[1]);
+
+        imprimir(matriz);
+        
+        System.out.println("Somando: "+ somatorio(matriz));
+
+        //OU    // DA NA MESMA
+
+        int negativos = qntdNegativos(matriz);
+
+        System.out.print("Negativos: " + negativos);
+       
+
+    }
+}
